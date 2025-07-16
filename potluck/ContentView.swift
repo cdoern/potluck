6fd2626206fbcd8e8dcc2918cf928ebce102a566
Charlie Doern <cdoern@redhat.com>
2025-07-15 21:11:20 -0400

    Initial Commit
    Signed-off-by: Charlie Doern <cdoern@redhat.com>

---
+++ potluck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  potluck
+//
+//  Created by Charlie Doern on 7/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ potluck/potluckApp.swift
@@ -0,0 +1,17 @@
+//
+//  potluckApp.swift
+//  potluck
+//
+//  Created by Charlie Doern on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct potluckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
